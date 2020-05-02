79efb02131e1ffe81878e2b659cb357134802088
Scarlet <developerscarlet@gmail.com>
2020-05-01 21:03:16 -0400

    Added User Login and Registration.

---
+++ MathSeer.UI/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MathSeer.UI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MathSeer.UI.Controllers
+{
+    [Route("auth/")]
+    public class AuthController : Controller
+    {
+        [HttpGet("login")]
+        public IActionResult LoginPage()
+        {
+            return View(new UserLogin());
+        }
+
+        [HttpGet("register")]
+        public IActionResult RegisterPage()
+        {
+            return View(new UserRegister());
+        }
+    }
+}
---
+++ MathSeer.UI/Models/UserLogin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MathSeer.UI.Models
+{
+    public class UserLogin
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
