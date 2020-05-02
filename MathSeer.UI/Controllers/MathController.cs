3c11b626cc2056f1445f41706c2421da38ca2bb3
Scarlet <developerscarlet@gmail.com>
2020-05-01 21:12:42 -0400

    Added MathController. Will need to add sub controller for algerba and trig.

---
+++ MathSeer.UI/Controllers/MathController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MathSeer.UI.Controllers
+{
+    public class MathController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
