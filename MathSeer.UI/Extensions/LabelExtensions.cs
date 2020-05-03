2ba49481a06f736204d444ca652cd0c91a1ec427
Scarlet <developerscarlet@gmail.com>
2020-05-03 18:44:52 -0400

    Added Extensions library.

---
+++ MathSeer.UI/Extensions/LabelExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace MathSeer.UI.Extensions
+{
+    public static class LabelExtensions
+    {
+        //public static HtmlHelper LabelWithLink(this HtmlHelper html, string label, object linkAttributes)
+        //{
+
+        //}
+    }
+}
