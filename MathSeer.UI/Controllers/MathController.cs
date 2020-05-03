557521e22ac432e715353463057289f8af425153
Scarlet <developerscarlet@gmail.com>
2020-05-02 22:50:09 -0400

    Added algebra and trig actions to math controller.

--- MathSeer.UI/Controllers/MathController.cs
+++ MathSeer.UI/Controllers/MathController.cs
@@ -6,9 +6,22 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MathSeer.UI.Controllers
 {
+    [Route("/math")]
     public class MathController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet, Route("/algebra")]
+        public IActionResult Algebra()
+        {
+            return View();
+        }
+
+        [HttpGet, Route("/trigs")]
+        public IActionResult Trig()
         {
             return View();
         }
