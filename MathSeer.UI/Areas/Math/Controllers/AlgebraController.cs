60f69258ff1be6f5594d858ea3f05cb0af738b98
Scarlet <developerscarlet@gmail.com>
2020-06-13 23:22:30 -0400

    Add Algebra Controller to Math Area

---
+++ MathSeer.UI/Areas/Math/Controllers/AlgebraController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MathSeer.UI.Areas.Math.Controllers
+{
+    [Route("/algebra")]
+    public class AlgebraController : Controller
+    {
+        [Route("/home")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Editor()
+        {
+            return View();
+        }
+
+        public IActionResult Answersheet()
+        {
+            return View();
+        }
+    }
+}
