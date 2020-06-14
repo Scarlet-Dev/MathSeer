c3f92fc3d5f6bc311cdacd39cdd566b669301d7d
Scarlet <developerscarlet@gmail.com>
2020-06-13 23:21:29 -0400

    Add Services Extension class for Repo usage

---
+++ MathSeer.UI/Extensions/ServicesExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MathSeer.UI.Extensions
+{
+    public class ServicesExtensions : IServiceProvider
+    {
+        public object GetService(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddClaims(ServiceCollection services)
+        {
+
+        }
+
+        public void AddRepository(ServiceCollection services)
+        {
+
+        }
+    }
+}
