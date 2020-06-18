5377879de39b88eaab1dfe6f14c2db7479724dd5
Scarlet <developerscarlet@gmail.com>
2020-06-17 23:15:37 -0400

    Add MathRepoBase

---
+++ MathSeer.DataAccess/Interfaces/Repository/IMathRepositoryBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathSeer.DataAccess.Interfaces
+{
+    public interface IMathRepositoryBase : IRepositoryBase
+    {
+        public string[] ParseExpression();
+
+        public void Calculate();
+    }
+}
